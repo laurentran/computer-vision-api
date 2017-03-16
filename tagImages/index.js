08ea80a2f828004f035c750135a2de0d09770572
laurentran <lauren.tran@microsoft.com>
2017-03-16 12:34:27 -0700

    Update index.js

--- tagImages/index.js
+++ tagImages/index.js
@@ -1,16 +1,14 @@
 request = require('request')
 
 module.exports = function (context, myBlob) {
-    
-    context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
-    
+        
+    // get blob storage image URL 
     var baseUrl = process.env.BLOB_STORAGE_URL;
     var url = baseUrl + context.bindingData.name;
-    context.log(url);
 
     requestBody = JSON.stringify({"url":url})
 
-    // ping API
+    // ping Computer Vision API
     request.post({
         headers: {'content-type' : 'application/json', 'Ocp-Apim-Subscription-Key' : process.env.API_KEY},
         url: 'https://westus.api.cognitive.microsoft.com/vision/v1.0/tag',
@@ -20,6 +18,8 @@ module.exports = function (context, myBlob) {
             context.log(error);
         } else {
             var results = JSON.parse(body).tags;
+            
+            // save tags that exceed confidence threshold of 0.7
             var tags = [];
             for (var i = 0; i < results.length; i++) {
                 var result = results[i];
@@ -29,8 +29,8 @@ module.exports = function (context, myBlob) {
                     tags.push(tag);
                 }
             }
-            context.log(tags);
-            
+
+            // write to Azure Table Storage
             context.bindings.outputTable = {
                 "partitionKey": "image-metadata",
                 "rowKey": context.bindingData.name,
